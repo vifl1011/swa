d80f77a39e4ae11101df01bc0ca47242b4b2f50c
Florian Vieser <vifl1011 (at) hs-karlsruhe.de>
2013-04-04 09:20:01 +0200

    Adresse INSERTs erstellt

---
+++ shop/src/main/resources/sql/22_adresse.sql
@@ -0,0 +1,10 @@
+insert into adresse (id, kunde_id, ort, plz, strasse, hausnummer, erzeugt, aktualisiert) values (400,200,'Karlsruhe',76131,'Sebastian-Kneipp-Straße',1,'19.10.2012 00:00:00','19.10.2012 00:00:00');
+insert into adresse (id, kunde_id, ort, plz, strasse, hausnummer, erzeugt, aktualisiert) values (401,201,'Karlsruhe',76131,'Sebastian-Kneipp-Straße',2,'19.10.2012 00:00:00','19.10.2012 00:00:00');
+insert into adresse (id, kunde_id, ort, plz, strasse, hausnummer, erzeugt, aktualisiert) values (402,202,'Karlsruhe',76131,'Sebastian-Kneipp-Straße',3,'19.10.2012 00:00:00','19.10.2012 00:00:00');
+insert into adresse (id, kunde_id, ort, plz, strasse, hausnummer, erzeugt, aktualisiert) values (403,203,'Karlsruhe',76131,'Sebastian-Kneipp-Straße',4,'19.10.2012 00:00:00','19.10.2012 00:00:00');
+insert into adresse (id, kunde_id, ort, plz, strasse, hausnummer, erzeugt, aktualisiert) values (404,204,'Karlsruhe',76131,'Sebastian-Kneipp-Straße',5,'19.10.2012 00:00:00','19.10.2012 00:00:00');
+insert into adresse (id, kunde_id, ort, plz, strasse, hausnummer, erzeugt, aktualisiert) values (405,205,'Freiburg',79098,'Bismarck-Allee',1,'19.10.2012 00:00:00','19.10.2012 00:00:00');
+insert into adresse (id, kunde_id, ort, plz, strasse, hausnummer, erzeugt, aktualisiert) values (406,206,'Freiburg',79098,'Bismarck-Allee',2,'19.10.2012 00:00:00','19.10.2012 00:00:00');
+insert into adresse (id, kunde_id, ort, plz, strasse, hausnummer, erzeugt, aktualisiert) values (407,207,'Freiburg',79098,'Bismarck-Allee',3,'19.10.2012 00:00:00','19.10.2012 00:00:00');
+insert into adresse (id, kunde_id, ort, plz, strasse, hausnummer, erzeugt, aktualisiert) values (408,208,'Freiburg',79098,'Bismarck-Allee',4,'19.10.2012 00:00:00','19.10.2012 00:00:00');
+insert into adresse (id, kunde_id, ort, plz, strasse, hausnummer, erzeugt, aktualisiert) values (409,209,'Freiburg',79098,'Bismarck-Allee',5,'19.10.2012 00:00:00','19.10.2012 00:00:00');
