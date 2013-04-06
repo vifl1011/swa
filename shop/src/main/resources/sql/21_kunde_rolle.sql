8c37ddb1e2a71b1b05ca9f386c91aa56e32a3c56
Patrik Steuer <stpa1017 (at) hs-karlsruhe.de>
2013-04-06 20:46:03 +0200

    ein paar rollen inserts hinzugefügt ...

---
+++ shop/src/main/resources/sql/21_kunde_rolle.sql
@@ -0,0 +1,18 @@
+-- ===============================================================================
+-- Jede SQL-Anweisung muss in genau 1 Zeile
+-- Kommentare durch -- am Zeilenanfang
+-- ===============================================================================
+
+
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (200,0);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (200,1);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (200,2);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (200,3);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (201,0);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (201,1);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (201,3);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (202,1);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (202,3);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (203,1);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (203,3);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (204,3);
