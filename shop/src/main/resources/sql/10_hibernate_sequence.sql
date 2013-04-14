3e0e423c2fdca56b42fbcf334813c1525991f749
Florian Vieser <vifl1011 (at) hs-karlsruhe.de>
2013-04-14 17:51:54 +0200

    hibernate_sequence.sql ergänzt, da diese nicht vorhanden war.

---
+++ shop/src/main/resources/sql/10_hibernate_sequence.sql
@@ -0,0 +1,7 @@
+-- ===============================================================================
+-- Jede SQL-Anweisung muss in genau 1 Zeile
+-- Kommentare durch -- am Zeilenanfang
+-- ===============================================================================
+
+DROP SEQUENCE hibernate_sequence;
+CREATE SEQUENCE hibernate_sequence START WITH 5000;
