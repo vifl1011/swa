bcc6876112fcde3ce274a08819693a08ff21db07
Patrik Steuer <stpa1017 (at) hs-karlsruhe.de>
2013-05-29 14:00:02 +0200

    Spracheändern ist nun möglich, Login schon eingebaut tut aber noch net

--- shop/src/main/java/de/shop/util/SpracheListener.java
+++ shop/src/main/java/de/shop/util/SpracheListener.java
@@ -1,6 +1,7 @@
 package de.shop.util;
 
 import java.io.Serializable;
+import java.lang.invoke.MethodHandles;
 import java.util.Locale;
 
 import javax.annotation.PostConstruct;
@@ -11,12 +12,16 @@ import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.jboss.logging.Logger;
+
 
 @Named("sprache")
 @SessionScoped
 @Log
 public class SpracheListener implements Serializable {
 	private static final long serialVersionUID = 1986565724093259408L;
+
+	private static final Logger LOGGER=Logger.getLogger(MethodHandles.lookup().lookupClass());
 	
 	@Produces
 	@Named
@@ -35,11 +40,13 @@ public class SpracheListener implements Serializable {
 	}
 
 	public void change(String localeStr) {
+		LOGGER.debugf("Locale_change: %s", localeStr);
 		final Locale newLocale = new Locale(localeStr);
 		if (newLocale.equals(locale)) {
 			return;
 		}
-		
+
+		LOGGER.debugf("Locale_change: setzen");
 		locale = newLocale;
 		ctx.getViewRoot().setLocale(locale);
 		ctx.renderResponse();
