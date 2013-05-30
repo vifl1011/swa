3486d742d97108f21985e9ce9027aea0158915f3
Fabio Wehmer <wefa1015@hs-karlsruhe.de>
2013-05-30 16:21:17 +0200

    CreateKunde Basis

--- shop/src/main/java/de/shop/kundenverwaltung/service/KundeServiceException.java
+++ shop/src/main/java/de/shop/kundenverwaltung/service/KundeServiceException.java
@@ -1,8 +1,8 @@
 package de.shop.kundenverwaltung.service;
 
-import de.shop.util.ShopException;
+import de.shop.util.AbstractShopException;
 
-public abstract class KundeServiceException extends ShopException {
+public abstract class KundeServiceException extends AbstractShopException {
 	private static final long serialVersionUID = -2849585609393128387L;
 
 	public KundeServiceException(String msg) {
