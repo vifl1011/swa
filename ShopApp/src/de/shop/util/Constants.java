2a6d0d442ef10ca8714fd128f2ebe1d321f33d06
Tobias Weigel <Tobi.Wee@googlemail.com>
2013-06-10 19:01:42 +0200

    Eine Konstante zum abspeichern für die IP-Adresse des Webservers im lokalen Netzwerk hinterlegt. --> Notwendig um zB vom Tablet aus Zugriff auf die Webservices zu erhalten (die emulierte IP-Adresse funktioniert nur für den Emulator)

--- ShopApp/src/de/shop/util/Constants.java
+++ ShopApp/src/de/shop/util/Constants.java
@@ -32,6 +32,7 @@ public final class Constants {
 	
 	public static final String PROTOCOL_DEFAULT = "http";
 	public static final String LOCALHOST_EMULATOR = "10.0.2.2";
+	public static final String LOCALHOST_DEVICE = "192.168.173.246"; //<---The IP Adress of the running Server in your network
 	public static final String HOST_DEFAULT = LOCALHOST_EMULATOR;
 	public static final String PORT_DEFAULT = "8080";
 	public static final String PATH_DEFAULT = "/shop/rest";
